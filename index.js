a1c4cef2149b48577c1a73df6199126ef79dc47d
malak jabr <malakjabr2@gmail.com>
2023-10-15 19:27:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+function superbowlWin(array){
+   const winningGame = array.find(game => game.result === "W")
+   if (winningGame){
+    return winningGame.year
+   } else {
+    return undefined
+   }
+   
+}
+superbowlWin(record)
